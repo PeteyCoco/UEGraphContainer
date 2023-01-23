222b7a0fa6950a71d3f7eecd038ba254e8aeebec
PeteyCoco <peter.carsten.collins@gmail.com>
2023-01-23 11:04:20 -0500

    PASS: Graph is created equal

--- Source/GraphContainer/Private/Tests/DirectedWeightedGraphTest.cpp
+++ Source/GraphContainer/Private/Tests/DirectedWeightedGraphTest.cpp
@@ -1,8 +1,33 @@
 #include "Misc/AutomationTest.h"
 #include "Tests/AutomationEditorCommon.h"
 
+template<typename BundledVertex, typename BundledEdge>
+class TDirectedWeightedGraph
+{
+public:
+	bool IsEmpty() const { return true; }
+};
+
+class FTestVertex
+{
+
+};
+
+class FTestEdge
+{
+
+};
+
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FDirectedWeightedGraph, "Graph Container.Directed Weighted Graph", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 bool FDirectedWeightedGraph::RunTest(FString const& Parameters) {
+
+	// Graph is created empty
+	{
+		TDirectedWeightedGraph<FTestVertex, FTestEdge> graph{};
+
+		TestTrue(TEXT("Graph is created empty"), graph.IsEmpty());
+	}
+
 	return true;
 }
