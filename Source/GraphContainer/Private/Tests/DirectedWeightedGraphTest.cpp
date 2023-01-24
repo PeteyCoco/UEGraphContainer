d22027913ba0d2ed8d43a86dc02b79a3ba04ad78
PeteyCoco <peter.carsten.collins@gmail.com>
2023-01-24 09:49:51 -0500

    PASS: Adding vertex increases graph size to one

--- Source/GraphContainer/Private/Tests/DirectedWeightedGraphTest.cpp
+++ Source/GraphContainer/Private/Tests/DirectedWeightedGraphTest.cpp
@@ -2,32 +2,62 @@
 #include <lemon/list_graph.h>
 #include "Tests/AutomationEditorCommon.h"
 
-template<typename BundledVertex, typename BundledEdge>
-class TDirectedWeightedGraph
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FDirectedWeightedGraph, "Graph Container.Directed Weighted Graph", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+class TestVertex
 {
 public:
+	// Set the ID of the Vertex
+	void SetID(int value) { ID = value; }
+
+private:
+	int ID{};
+};
+
+class TestEdge
+{
+
+};
+
+template <typename VertexClass, typename EdgeClass>
+class DirectedWeightedGraph
+{
+public:
+
+	// Checks whether the graph contains no nodes
 	bool IsEmpty() const { return true; }
+
+	// Counts the number of vertices in the graph
+	int NumVertices() const { return lemon::countNodes(graph); }
+
+	// Adds a node to the graph
+	void AddVertex(TestVertex& Vertex)
+	{
+		auto Node = graph.addNode();
+		Vertex.SetID(graph.id(Node));
+	}
+
+private:
+	lemon::ListDigraph graph{};
 };
-
-class FTestVertex
-{
-
-};
-
-class FTestEdge
-{
-
-};
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FDirectedWeightedGraph, "Graph Container.Directed Weighted Graph", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 bool FDirectedWeightedGraph::RunTest(FString const& Parameters) {
 
 	// Graph is created empty
 	{
-		lemon::ListDigraph h{};
+		DirectedWeightedGraph<TestVertex, TestEdge> graph;
 
-		TestTrue(TEXT("Graph is created empty"), lemon::countNodes(h) == 0);
+		TestTrue(TEXT("Graph is created empty"), graph.IsEmpty());
+	}
+
+	// Adding node increases the size of the graph to one
+	{
+		DirectedWeightedGraph<TestVertex, TestEdge> graph;
+		TestVertex V{};
+
+		graph.AddVertex(V);
+
+		TestEqual(TEXT("Graph must contain one node"), graph.NumVertices(), 1);
 	}
 
 	return true;
--- Source/ThirdParty/Lemon/Lemon.Build.cs
+++ Source/ThirdParty/Lemon/Lemon.Build.cs
@@ -10,7 +10,6 @@ public class Lemon : ModuleRules
 		Type = ModuleType.External;
 
         PublicDefinitions.Add("LEMON_NO_UNUSED_LOCAL_TYPEDEF_WARNINGS = 0");
-        //PublicDefinitions.Add("GCC");
 
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
         PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "lemon.lib"));
