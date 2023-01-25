2155f7fc0e77e94c7a8e2c0f8769b94199007bab
PeteyCoco <peter.carsten.collins@gmail.com>
2023-01-25 10:39:56 -0500

    Moved the rest of the function bodies into the cpp file

--- Source/GraphContainer/Private/DirectedWeightedGraph.cpp
+++ Source/GraphContainer/Private/DirectedWeightedGraph.cpp
@@ -1,7 +1,37 @@
 #include "DirectedWeightedGraph.h"
 
 template<typename NodeClass, typename EdgeClass>
+void DirectedWeightedGraph<NodeClass, EdgeClass>::AddNode(const NodeClass& Node)
+{
+	if (HasNode(Node))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot add the same node twice. No node added to graph."));
+		return;
+	}
+
+	auto NodeHandle = graph.addNode();
+	NodeMap.Add(Node.ID, NodeHandle);
+}
+
+template<typename NodeClass, typename EdgeClass>
 inline bool DirectedWeightedGraph<NodeClass, EdgeClass>::HasNode(const NodeClass& Node) const
 {
 	return NodeMap.Contains(Node.ID);
 }
+
+template<typename NodeClass, typename EdgeClass>
+void DirectedWeightedGraph<NodeClass, EdgeClass>::AddEdge(const EdgeClass& Edge, const NodeClass& Origin, const NodeClass& Destination)
+{
+	auto o = graph.addNode();
+	auto d = graph.addNode();
+	auto EdgeHandle = graph.addArc(o, d);
+	EdgeMap.Add(Edge.ID, EdgeHandle);
+}
+
+template<typename NodeClass, typename EdgeClass>
+bool DirectedWeightedGraph<NodeClass, EdgeClass>::HasEdge(const EdgeClass& Edge) const
+{
+	return EdgeMap.Contains(Edge.ID);
+}
+
+
--- Source/GraphContainer/Public/DirectedWeightedGraph.h
+++ Source/GraphContainer/Public/DirectedWeightedGraph.h
@@ -14,42 +14,23 @@ public:
 	typedef int FNodeRef;
 
 	// Checks whether the graph contains no nodes
-	bool IsEmpty() const { return true; }
+	inline bool IsEmpty() const { return true; }
 
 	// Counts the number of nodes in the graph
-	int NumNodes() const { return lemon::countNodes(graph); }
+	inline int NumNodes() const { return lemon::countNodes(graph); }
 
 	// Adds a node to the graph
-	void AddNode(const NodeClass& Node)
-	{
-		if (HasNode(Node))
-		{
-			UE_LOG(LogTemp, Error, TEXT("Cannot add the same node twice. No node added to graph."));
-			return;
-		}
-
-		auto NodeHandle = graph.addNode();
-		NodeMap.Add(Node.ID, NodeHandle);
-	}
+	void AddNode(const NodeClass& Node);
 
 	// Check if the graph contains the given node
 	bool HasNode(const NodeClass& Node) const;
 
 	// Adds an edge to the graph from the origin node to the destination node
-	void AddEdge(const EdgeClass& Edge, const NodeClass& Origin, const NodeClass& Destination)
-	{
-		auto o = graph.addNode();
-		auto d = graph.addNode();
-		auto EdgeHandle = graph.addArc(o, d);
-		EdgeMap.Add(Edge.ID, EdgeHandle);
-	}
+	void AddEdge(const EdgeClass& Edge, const NodeClass& Origin, const NodeClass& Destination);
+
 
 	// Check if the graph contains the given edge
-	bool HasEdge(const EdgeClass& Edge) const
-	{
-		return EdgeMap.Contains(Edge.ID);
-	}
-
+	bool HasEdge(const EdgeClass& Edge) const;
 
 private:
 	lemon::ListDigraph graph{};
